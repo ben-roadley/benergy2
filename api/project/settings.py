f54e1c1a6800360071a622d8f57bf648f29dfc9c
Ben Roadley <ben.roadley@gmail.com>
2026-06-02 21:55:26 +0200

    Add CORS support to Django settings and requirements

--- api/project/settings.py
+++ api/project/settings.py
@@ -32,6 +32,10 @@ CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS", "http://localhost"
     " "
 )
 
+CORS_ALLOWED_ORIGINS = os.environ.get("CORS_ALLOWED_ORIGINS", "http://localhost:8081").split(
+    " "
+)
+
 USE_X_FORWARDED_HOST = True
 
 # Because we're using nginx as a reverse proxy, we need to disable the default
@@ -47,6 +51,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'corsheaders',
     "rest_framework",
     "drf_spectacular",
     "users",
@@ -57,6 +62,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
