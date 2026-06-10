417391424bf107ab0aca5bacda1c7de47174fc2f
Ben Roadley <ben.roadley@gmail.com>
2026-06-10 13:48:08 +0200

    Move user & auth code to auth & users domains

---
+++ fastapi/src/auth/auth.py
@@ -0,0 +1,96 @@
+import os
+from datetime import datetime, timedelta, timezone
+from typing import Annotated
+
+import jwt
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from pwdlib import PasswordHash
+from pydantic import BaseModel
+
+from src.users.users import fake_users_db, User, UserInDB, get_user
+
+SECRET_KEY = os.getenv("FASTAPI_SECRET_KEY")
+ALGORITHM = os.getenv("FASTAPI_ALGORITHM", "HS256")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("FASTAPI_ACCESS_TOKEN_EXPIRE_MINUTES", 30))
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str | None = None
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+password_hash = PasswordHash.recommended()
+
+DUMMY_HASH = password_hash.hash("dummypassword")
+
+
+def verify_password(plain_password, hashed_password):
+    return password_hash.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return password_hash.hash(password)
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def authenticate_user(username: str, password: str):
+    # When authenticate_user is called with a username that doesn't exist in the database, 
+    # we still run verify_password against a dummy hash.
+
+    # This ensures the endpoint takes roughly the same amount of time to respond
+    # whether the username is valid or not, preventing timing attacks that
+    # could be used to enumerate existing usernames.
+    user = get_user(username)
+    if not user:
+        verify_password(password, DUMMY_HASH)
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except jwt.InvalidTokenError:
+        raise credentials_exception
+    user = get_user(username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+async def get_current_active_user(
+    current_user: Annotated[User, Depends(get_current_user)],
+):
+    if not current_user.is_active:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
--- fastapi/src/main.py
+++ fastapi/src/main.py
@@ -1,156 +1,28 @@
-from datetime import datetime, timedelta, timezone
+from datetime import timedelta
 from typing import Annotated
 
 import jwt
 from fastapi import Depends, FastAPI, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from pwdlib import PasswordHash
-from pydantic import BaseModel
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlmodel import Session
 
 from src.database import get_session
-# from src.workouts.models import AuthUser as User
 from src.workouts.schemas import WorkoutBaseSchema
 from src.workouts.services import get_workouts
 
-SECRET_KEY = "c58c902ec390d3c3f96f2efdaa1794c4eb5ebd112440184961724a3cdf6518ab"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-
-fake_users_db = {
-    "johndoe": {
-        "username": "johndoe",
-        "email": "johndoe@example.com",
-        "hashed_password": "$argon2id$v=19$m=65536,t=3,p=4$CsV2TNObzkAhxPmcC5oh8w$B4iETvqbQMqmq+mvUhlMGaHwHvnk1biMGvEzsbdkVGA",
-        "is_active": True,
-    },
-    "alice": {
-        "username": "alice",
-        "email": "alice@example.com",
-        "hashed_password": "fakehashedsecret2",
-        "is_active": False,
-    },
-}
-
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-
-
-class TokenData(BaseModel):
-    username: str | None = None
+from src.auth.auth import Token, User, authenticate_user, create_access_token, get_current_active_user, ACCESS_TOKEN_EXPIRE_MINUTES
 
 
 app = FastAPI()
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-
-def fake_hash_password(password: str):
-    return "fakehashed" + password
-
-password_hash = PasswordHash.recommended()
-
-DUMMY_HASH = password_hash.hash("dummypassword")
-print("PASSWORD HASH")
-print(password_hash.hash("secret"))
-
-def verify_password(plain_password, hashed_password):
-    print(f"Verifying password: {plain_password} against hash: {hashed_password}")
-    return password_hash.verify(plain_password, hashed_password)
-
-
-def get_password_hash(password):
-    return password_hash.hash(password)
-
-
-class User(BaseModel):
-    username: str
-    email: str | None = None
-    is_active: bool | None = None
-
-
-class UserInDB(User):
-    hashed_password: str
-
-
-
-
-def get_user(db, username: str):
-    if username in db:
-        user_dict = db[username]
-        return UserInDB(**user_dict)
-
-
-
-def authenticate_user(fake_db, username: str, password: str):
-    # When authenticate_user is called with a username that doesn't exist in the database, 
-    # we still run verify_password against a dummy hash.
-
-    # This ensures the endpoint takes roughly the same amount of time to respond
-    # whether the username is valid or not, preventing timing attacks that
-    # could be used to enumerate existing usernames.
-    user = get_user(fake_db, username)
-    if not user:
-        verify_password(password, DUMMY_HASH)
-        return False
-    if not verify_password(password, user.hashed_password):
-        return False
-    return user
-
-
-def fake_decode_token(token):
-    # This doesn't provide any security at all
-    # Check the next version
-    user = get_user(fake_users_db, token)
-    return user
-
-
-def create_access_token(data: dict, expires_delta: timedelta | None = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.now(timezone.utc) + expires_delta
-    else:
-        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
-
-
-async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        token_data = TokenData(username=username)
-    except jwt.InvalidTokenError:
-        raise credentials_exception
-    user = get_user(fake_users_db, username=token_data.username)
-    if user is None:
-        raise credentials_exception
-    return user
-
-
-async def get_current_active_user(
-    current_user: Annotated[User, Depends(get_current_user)],
-):
-    if not current_user.is_active:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    return current_user
-
+# Authentication endpoints
 
 @app.post("/token")
 async def login_for_access_token(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
 ) -> Token:
-    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    user = authenticate_user(form_data.username, form_data.password)
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -164,6 +36,8 @@ async def login_for_access_token(
     return Token(access_token=access_token, token_type="bearer")
 
 
+# User endpoints
+
 @app.get("/users/me/")
 async def read_users_me(
     current_user: Annotated[User, Depends(get_current_active_user)],
@@ -171,15 +45,14 @@ async def read_users_me(
     return current_user
 
 
+# Workout endpoints
 
-
-############################
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
 
 @app.get("/workouts/", response_model=list[WorkoutBaseSchema])
 def read_items(session: Session = Depends(get_session)):
     user_id = 2
     return get_workouts(session, user_id)
-#######################
